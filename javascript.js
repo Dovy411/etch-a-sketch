436321832bc3cce6fac8a999c7297281481619b6
Dovy4111 <102235946+Dovy411@users.noreply.github.com>
2024-11-10 16:48:23 +0000

    Mostly finished. No maximum user input set and no color changing nor darkening

--- javascript.js
+++ javascript.js
@@ -3,16 +3,34 @@ const container = document.querySelector("#container");
 // let box = document.createElement("div");
 // box.classList.add("box");
 
+function changeBoxNumber() {
+    let gridItems = prompt("Enter a number: ");
+    
+    
+    while (container.firstChild) {
+        container.firstChild.remove()
+    }
 
+    
 
-
-for (let i = 0; i < 256; i++) {
-    let box = document.createElement("div");
-    box.classList.add("box");
-    container.appendChild(box);
-
-    box.addEventListener("mouseover", () => {
-        box.style.backgroundColor = "red";
-    })
+    for (let i = 0; i < gridItems*gridItems; i++) {
+        let box = document.createElement("div");
+        box.style.flexBasis = `calc(100% / ${gridItems})`;
+        box.classList.add("box");
+        container.appendChild(box);
+    
+        box.addEventListener("mouseover", () => {
+            box.style.backgroundColor = "red";
+        })
+    }
 }
+
+
+const button = document.querySelector("#button");
+button.addEventListener ("click", () => {
+    changeBoxNumber();
+})
+
+
+
 
